a2336a9d91a309e7f357f3ccf11153b7146ceea9
Niloufar <116565518+Niloufar28@users.noreply.github.com>
2023-05-22 17:27:21 +0430

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let $ = document
+
+const inputElem = $.querySelector("input")
+const spanElem = $.querySelector(".counter")
+const inputMaxLength = inputElem.getAttribute("maxlength")
+
+inputElem.addEventListener("keyup", function (){
+    spanElem.innerHTML = inputMaxLength - inputElem.value.length
+
+})
